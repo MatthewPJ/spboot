e901fc0fb2f1cc3631a3efc40fa9909ccacfa8ef
matthew <524948894@qq.com>
2019-04-19 00:13:25 +0800

    ci

--- ci/task/build-task.sh
+++ ci/task/build-task.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
+
 cd spboot
 mvn package
-cp spboot/target/spboot-0.0.1-SNAPSHOT.jar build-output/app.jar
-ls build-output
+#cp spboot/target/spboot-0.0.1-SNAPSHOT.jar build-output/app.jar
+#ls build-output
--- ci/task/deploy-task.sh
+++ ci/task/deploy-task.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+
 cd build-output
 jara -jar app.jar
